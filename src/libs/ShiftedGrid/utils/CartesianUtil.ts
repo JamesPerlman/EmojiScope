7dedc680bca56996507d7d8affdf6749692c6c25
JamesPerlman <jam.e.perl@gmail.com>
2021-07-14 21:56:50 -0700

    remove debug code from CartesianUtil.ts

--- src/libs/ShiftedGrid/utils/CartesianUtil.ts
+++ src/libs/ShiftedGrid/utils/CartesianUtil.ts
@@ -32,18 +32,3 @@ export function getCartesianDistance(a: Point2D, b: Point2D): number {
   const dy = b.y - a.y;
   return Math.sqrt(dx * dx + dy * dy);
 }
-
-
-const gridPoint0 = { x: 23, y: 42 };
-const gridPoint1 = { x: -69, y: 69 };
-const gridPoint2 = { x: 1337, y: 420 };
-
-const cartPoint0 = gridToCart(gridPoint0);
-const cartPoint1 = gridToCart(gridPoint1);
-const cartPoint2 = gridToCart(gridPoint2);
-
-const p0 = cartToGrid(cartPoint0); // should = gridPoint0
-const p1 = cartToGrid(cartPoint1); // should = gridPoint1
-const p2 = cartToGrid(cartPoint2); // should = gridPoint2
-
-console.log('f');
