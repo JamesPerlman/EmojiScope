c4603be7a568cfb3641654fb72c2e9f4ca17d5d3
JamesPerlman <jam.e.perl@gmail.com>
2021-07-18 13:31:39 -0700

    remove unnecessary debug code

--- src/libs/ShiftedGrid/utils/CartesianUtil.ts
+++ src/libs/ShiftedGrid/utils/CartesianUtil.ts
@@ -27,38 +27,9 @@ export function cartToGrid({ x: gx, y: gy }: Point2D): Index2D {
 }
 
 // Get linear cartesian distance between points a and b
+// check this out btw https://jsben.ch/stsBd
 export function getCartesianDistance(a: Point2D, b: Point2D): number {
   const dx = b.x - a.x;
   const dy = b.y - a.y;
   return Math.sqrt(dx * dx + dy * dy);
 }
-
-const a1 = { x: 0, y: 1 };
-const a2 = { x: 0, y: 2 };
-const a3 = { x: 0, y: 3 };
-const a4 = { x: 0, y: 4 };
-const a5 = { x: 0, y: 5 };
-const a6 = { x: 0, y: 6 };
-
-const c1 = { x: 0, y: 1 };
-const c2 = { x: 0, y: 2 };
-const c3 = { x: 0, y: 3 };
-const c4 = { x: 0, y: 4 };
-const c5 = { x: 0, y: 5 };
-const c6 = { x: 0, y: 6 };
-
-const d1 = gridToCart(a1);
-const d2 = gridToCart(a2);
-const d3 = gridToCart(a3);
-const d4 = gridToCart(a4);
-const d5 = gridToCart(a5);
-const d6 = gridToCart(a6);
-
-const g1 = cartToGrid(d1);
-const g2 = cartToGrid(d2);
-const g3 = cartToGrid(d3);
-const g4 = cartToGrid(d4);
-const g5 = cartToGrid(d5);
-const g6 = cartToGrid(d6);
-
-console.log('hey!');
