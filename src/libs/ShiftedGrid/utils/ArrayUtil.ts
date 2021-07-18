a200c9150548d2aa3c661f53dd92333024ba2f3f
JamesPerlman <jam.e.perl@gmail.com>
2021-07-17 23:03:12 -0700

    delete obsolete code

--- src/libs/ShiftedGrid/utils/ArrayUtil.ts
+++
@@ -1,4 +0,0 @@
-// filter functions, for example
-export function isIndexEven(_: any, i: number) {
-  return i % 2 === 0;
-}
--- src/libs/ShiftedGrid/utils/CompareUtil.ts
+++
@@ -1,7 +0,0 @@
-export function objectMin<T>(a: T, b: T, getComparisonValue: (o: T) => number): T {
-  if (getComparisonValue(a) < getComparisonValue(b)) {
-    return a;
-  } else {
-    return b;
-  }
-}
